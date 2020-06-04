15f0e96ed2dab65e9994906f444b261596673b84
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-06-04 19:46:58 +0100

    Add files via upload

---
+++ ConsoleColor Object.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program
+{
+    class Color
+    {
+        static public void Run()
+        {
+            Console.Title = "By Denis Rafi";
+            string input = "";
+            ConsoleColor myColor = new ConsoleColor();
+            myColor = ConsoleColor.Blue;
+            input = Console.ReadLine();
+            ColorWriteLine(myColor, input);
+            Console.ReadKey();
+        }
+        static void ColorWriteLine(ConsoleColor theColor, string theMessage)
+        {
+            Console.ForegroundColor = theColor;
+            Console.WriteLine(theMessage);
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Color.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
+
